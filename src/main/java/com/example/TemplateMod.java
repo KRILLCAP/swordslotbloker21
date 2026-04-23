194727f643f9fb6ef3d0dc25f141b622d1587f09
KRILLCAP <kirillca1234554321@gmail.com>
2026-04-23 17:14:01 +0500

    Update TemplateMod.java

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -1,28 +1,10 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class TemplateMod implements ClientModInitializer {
-    private int lastSlot = -1;
-
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null) return;
-
-            int currentSlot = client.player.getInventory().selectedSlot;
-            boolean isHurt = client.player.hurtTime > 0;
-
-            // Если получили урон и слот изменился - откатываем
-            if (isHurt && lastSlot != -1 && lastSlot != currentSlot) {
-                client.player.getInventory().selectedSlot = lastSlot;
-            }
-
-            // Всегда запоминаем слот ДО возможной смены
-            if (!isHurt) {
-                lastSlot = client.player.getInventory().selectedSlot;
-            }
-        });
+        // Mixin теперь обрабатывает
     }
 }
