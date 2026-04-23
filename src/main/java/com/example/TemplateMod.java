d333d278d32532595988b4ef69f13a5ed964ba8b
KRILLCAP <kirillca1234554321@gmail.com>
2026-04-23 19:24:02 +0500

    Update TemplateMod.java

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class TemplateMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Mixin теперь обрабатывает
+        // Mixin сам всё обрабатывает
     }
 }
