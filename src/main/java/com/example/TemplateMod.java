5ac846cb8ab9281044f4558617d044fa8d7ab491
KRILLCAP <kirillca1234554321@gmail.com>
2026-04-23 20:21:44 +0500

    Update TemplateMod.java

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -1,13 +1,27 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class TemplateMod implements ClientModInitializer {
 
-    public static boolean lockSlot = false;
-
     @Override
     public void onInitializeClient() {
-        // Пусто, Mixin сам обрабатывает
+        // Сообщение при загрузке мода
+        System.out.println("[Confuse Blocker] Мод загружен!");
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) return;
+
+            long window = client.getWindow().getHandle();
+            boolean isPressed = GLFW.glfwGetKey(window, GLFW.GLFW_KEY_LEFT_CONTROL) == GLFW.GLFW_PRESS;
+
+            if (isPressed) {
+                client.player.sendMessage(Text.of("§a[Confuse Blocker] §fСлот ЗАБЛОКИРОВАН"), false);
+            }
+        });
     }
 }
